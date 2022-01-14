c8c6001d6896e871610ae05893e9e7382456f7e9
Rukul <rukulsingh98@gmail.com>
2022-01-14 14:22:37 +0530

    Added Temporary Soundtracks

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'My Time Is Now',
+        path: 'sounds/MyTimeIsNow.mp3',
+        artist: 'John Cena',
+        cover: 'images/cover1.png'
+    },
+    {
+        name: 'Golmaal Mix',
+        path: 'sounds/GolmaalTemperatureMix.mp3',
+        artist: 'DJ Vishal',
+        cover: 'images/cover2.png'
+    },
+    {
+        name: 'Jaata Da Chhora',
+        path: 'sounds/JaataDaChhora.mp3',
+        artist: 'Unknown',
+        cover: 'images/cover3.png'
+    },
+    {
+        name: 'Mauja Hi Mauja - Remix',
+        path: 'sounds/MaujaHiMaujaRemix.mp3',
+        artist: 'HollyBollyExpress.com',
+        cover: 'images/cover4.png'
+    },
+    {
+        name: 'Gasolina',
+        path: 'sounds/Gasolina.mp3',
+        artist: 'Unknown',
+        cover: 'images/cover5.png'
+    },
+]
